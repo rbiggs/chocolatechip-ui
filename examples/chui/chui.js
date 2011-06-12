dc613e36a2eda35473a18636002b9208725d2390
Robert Biggs <rbiggs@ymail.com>
2011-06-12 08:41:51 -0700

    Initial work for implementing spinners. This includes new ChUI.js, ChUI.css and a new icon for plus/minus accessor buttons.

--- examples/chui/chui.js
+++ examples/chui/chui.js
@@ -1250,6 +1250,98 @@ $.extend(HTMLElement.prototype, {
         }
     }
 });
+$.extend($, {
+	/*
+		option values:
+		selector:
+		name: 
+		range: {start:, end:, values: }
+		step:
+		defaultValue:
+		buttonClass:
+		indicator:
+	*/
+	UISpinner : function (opts) {
+		if (!!opts.selector) {
+			var spinner = $(opts.selector);
+		}
+		var step = opts.step;
+		if (opts.range.start >= 0) {
+			var rangeStart = opts.range.start || "";
+			var rangeEnd = opts.range.end || "";
+			var tempNum = rangeEnd - rangeStart;
+			tempNum++;
+			var range = [];
+			if (step) {
+				var mod = ((rangeEnd-rangeStart)/step);
+				if (opts.range.start == 0) {
+					range.push(0);
+				} else {
+					range.push(rangeStart);
+				}
+				for (var i = 1; i < mod; i++) {
+					range.push(range[i-1] + step);
+				}
+				range.push(range[range.length-1] + step);
+			} else {
+				for (var i = 0, tempNum; i < tempNum; i++) {
+					range.push(rangeStart + i);				
+				}
+			}
+		}
+		var icon = (opts.indicator === "plus") ? "<icon class='indicator'></icon>" : "<icon></icon>";
+		var buttonClass = opts.buttonClass ? " class='" + opts.buttonClass + "' " : "";
+		var decreaseButton = "<uibutton " + buttonClass + "ui-implements='icon'>" + icon + "</uibutton>";
+		var increaseButton = "<uibutton " + buttonClass + "ui-implements='icon'>" + icon + "</uibutton>";
+		var spinnerTemp = decreaseButton + "<label ui-kind='spinner-label'></label><input type='text'/>" + increaseButton;
+		spinner.insert(spinnerTemp);
+		if (opts.range.values) {
+			spinner.data("range-value", opts.range.values.join(","));
+		}
+		if (range) {
+			spinner.data("range-value", range.join(","));
+		}
+		if (opts.defaultValue) {
+			$("label[ui-kind=spinner-label]", spinner).text(opts.defaultValue);
+		}
+		$("uibutton:first-of-type", opts.selector).bind("click", function(button) {
+			$.decreaseSpinnerValue.call(this, opts.selector);
+		});
+		$("uibutton:last-of-type", opts.selector).bind("click", function(button) {
+			$.increaseSpinnerValue.call(this, opts.selector);
+		});
+	},
+	
+	decreaseSpinnerValue : function(selector) {
+		var values = $(selector).data("range-value");
+		values = values.split(",");
+		var defaultValue = $("label", selector).text().trim();
+		var idx = values.indexOf(defaultValue);
+		if (idx !== -1) {
+			$("uibutton:last-of-type", selector).removeClass("disabled");
+			$("[ui-kind=spinner-label]", selector).text(values[idx-1]);
+			$("input", selector).value = values[idx-1];
+			if (idx === 1) {
+				this.addClass("disabled");
+			} 
+		}	
+	},
+	
+	increaseSpinnerValue : function(selector) {
+		var values = $(selector).data("range-value");
+		values = values.split(",");
+		var defaultValue = $("label", selector).text().trim();
+		var idx = values.indexOf(defaultValue);
+		if (idx !== -1) {
+			$("uibutton:first-of-type", selector).removeClass("disabled");
+			$("label[ui-kind=spinner-label]", selector).text(values[idx+1]);
+			$("input", selector).value = values[idx+1];
+			if (idx === values.length-2) {
+				this.addClass("disabled");
+			}
+		}
+	}
+});
 
 $.extend($, {
     
