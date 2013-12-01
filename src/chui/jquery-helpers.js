1dafa2d2aa61e970cce61250c27754267d049e2a
Robert Biggs <robert.biggs@sourcebits.com>
2013-12-01 09:55:05 -0800

    Fixed bug in detectors.

--- src/chui/jquery-helpers.js
+++ src/chui/jquery-helpers.js
@@ -98,8 +98,8 @@
          isWebkit : navigator.userAgent.match(/webkit/),
          isMobile : /mobile/img.test(navigator.userAgent),
          isDesktop : !(/mobile/img.test(navigator.userAgent)),
-         isSafari : (!/Chrome/img.test(navigator.userAgent) && /Safari/img.test(navigator.userAgent) && !/android/img.test(navigator.userAgent),
-         isChrome : /chrome/i.test(navigator.userAgent),
-         isNativeAndroid : /android/i.test(navigator.userAgent) && /webkit/i.test(navigator.userAgent) && !/chrome/i.test(navigator.userAgent)
+         isSafari : (!/Chrome/img.test(navigator.userAgent) && /Safari/img.test(navigator.userAgent) && !/android/img.test(navigator.userAgent)),
+         isChrome : /Chrome/img.test(navigator.userAgent),
+         isNativeAndroid : (/android/i.test(navigator.userAgent) && /webkit/i.test(navigator.userAgent) && !/chrome/i.test(navigator.userAgent))
       });
    }
