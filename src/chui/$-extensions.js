07ad6b73f055636a0e8e89ef052b19e1fe8d1cad
Robert Biggs <robert.biggs@sourcebits.com>
2013-12-09 07:46:00 -0800

    Update for $.UIDeletable()
    Fixed $.UIDeletable to handle multiple detable lists in app. Now each
    Edit button is linked to its deletable list, allowing different
    articles to have their own deletables.

--- src/chui/$-extensions.js
+++ src/chui/$-extensions.js
@@ -204,36 +204,57 @@
          var deleteButton;
          var editButton;
          var deletionIndicator;
+         var button;
          // Windows uses an icon for the delete button:
          if ($.isWin) deleteLabel = '';
-         var setupDeletability = function(callback) {
+                  if (list[0].classList.contains('deletable')) return;
+         var height = $('li').eq(1)[0].clientHeight;
+         deleteButton = $.concat('<a href="javascript:void(null)" class="button delete">', deleteLabel, '</a>');
+         editButton = $.concat('<a href="javascript:void(null)" class="button edit">', editLabel, '</a>');
+         deletionIndicator = '<span class="deletion-indicator"></span>';
+         if (placement === 'left') {
+            list.closest('article').prev().prepend(editButton);
+         } else {
+            list.closest('article').prev().append(editButton);
+            list.closest('article').prev().find('h1').addClass('buttonOnRight');
+            list.closest('article').prev().find('.edit').addClass('align-flush');
+            button = list.closest('article').prev().find('.edit');
+         }
+         list.find('li').prepend(deletionIndicator);
+         list.find('li').append(deleteButton);
+         $('li').find('.delete').each(function(ctx, idx) {
+            if (window && window.jQuery && $ === window.jQuery) ctx = idx;
+            if ($.isiOS || $.isSafari) $(ctx).css({height: height + 'px'});
+         });
+         var setupDeletability = function(callback, list, button) {
             var deleteSlide;
+            console.dir(button);
             if ($.isiOS) {
                deleteSlide = '100px';
             } else if ($.isAndroid) {
                deleteSlide = '140px';
             }
             $(function() {
-               $.body.on('singletap', '.edit', function() {
+               button.on('singletap', function() {
                   var $this = this;
-                  list.addClass('deletable');
-                  setTimeout(function() {
-                     $this.classList.remove('edit');
-                     $this.classList.add('done');
-                     $($this).text(doneLabel);
-                     $(list).addClass('showIndicators');
-                  });
-               });
-               $.body.on('singletap', '.done', function() {
-                  var $this = this;
-                  list.removeClass('deletable');
-                  setTimeout(function() {
-                     $this.classList.remove('done');
-                     $this.classList.add('edit');
-                     $($this).text(editLabel);
-                     $(list).removeClass('showIndicators');
-                     $(list).find('li').removeClass('selected');
-                  });
+                  if (this.classList.contains('edit')) {
+                     list.addClass('deletable');
+                     setTimeout(function() {
+                        $this.classList.remove('edit');
+                        $this.classList.add('done');
+                        $($this).text(doneLabel);
+                        $(list).addClass('showIndicators');
+                     });
+                  } else if (this.classList.contains('done')) {
+                     list.removeClass('deletable');
+                     setTimeout(function() {
+                        $this.classList.remove('done');
+                        $this.classList.add('edit');
+                        $($this).text(editLabel);
+                        $(list).removeClass('showIndicators');
+                        $(list).find('li').removeClass('selected');
+                     });                    
+                  }
                });
                $(list).on('singletap', '.deletion-indicator', function() {
                   if ($(this).closest('li')[0].classList.contains('selected')) {
@@ -259,26 +280,8 @@
                });
             });         
          };
-         if (list[0].classList.contains('deletable')) return;
-            var height = $('li').eq(1)[0].clientHeight;
-            deleteButton = $.concat('<a href="javascript:void(null)" class="button delete">', deleteLabel, '</a>');
-            editButton = $.concat('<a href="javascript:void(null)" class="button edit">', editLabel, '</a>');
-            deletionIndicator = '<span class="deletion-indicator"></span>';
-            if (placement === 'left') {
-               list.closest('article').prev().prepend(editButton);
-            } else {
-               list.closest('article').prev().append(editButton);
-               list.closest('article').prev().find('h1').addClass('buttonOnRight');
-               list.closest('article').prev().find('.edit').addClass('align-flush');
-            }
-            list.find('li').prepend(deletionIndicator);
-            list.find('li').append(deleteButton);
-            $('li').find('.delete').each(function(ctx, idx) {
-               if (window && window.jQuery && $ === window.jQuery) ctx = idx;
-               if ($.isiOS || $.isSafari) $(ctx).css({height: height + 'px'});
-            });
-            setupDeletability(callback);
-         return list;
+         return setupDeletability(callback, list, button);
+         //return list;
       },
       
       ///////////////////////
