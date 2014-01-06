4b21b024fd6739399dbecee1c8dc79b36878b351
Simon Waldherr <git@simon.waldherr.eu>
2014-01-06 23:13:09 +0100

    prevent scrolling of complete document if the view is scrolled to the very top or bottom

---
+++ src/chui/scroll.js
@@ -0,0 +1,22 @@
+(function($) {
+  'use strict';
+  document.addEventListener('touchstart', function (e) {
+    var parent = e.target,
+      i = 0;
+
+    for (i = 0; i < 10; i += 1) {
+      if (parent !== null) {
+        if (parent.className !== undefined) {
+          if (parent.className.match('navigable')) {
+            if (parent.scrollTop === 0) {
+              parent.scrollTop = 1;
+            } else if ((parent.scrollTop + parent.offsetHeight) === parent.scrollHeight) {
+              parent.scrollTop = parent.scrollTop - 1;
+            }
+          }
+        }
+        parent = parent.parentNode;
+      }
+    }
+  });
+})(window.jQuery);
