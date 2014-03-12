0e2b9f6f168d364c441f828930d66d14df0900b5
sourcebits-rbiggs <robert.biggs@sourcebits.com>
2014-03-12 06:34:09 -0700

    Removed deprecated features from Gulpjs.
    Updated example images again.

--- gulpfile.js
+++ gulpfile.js
@@ -16,34 +16,34 @@ var gulp = require('gulp')
 
 // Define values for file headers:
 var chui = ['ChUI-Android.css','ChUI-iOS.css','ChUI-Win.css','ChUI.js']
-,		osNames = ['Android','iOS','Windows']
-,		osTypes = ['android','ios','win'];
+,   osNames = ['Android','iOS','Windows']
+,   osTypes = ['android','ios','win'];
 
 // Define header for ChUI files:
 var chuiHeader = ['/*',
-	'    pO\\',
-	'   6  /\\',
-	'     /OO\\',
-	'    /OOOO\\',
-	'   /OOOOOO\\',
-	'  (OOOOOOOO)',
-	'   \\:~==~:/',
-	'',
-	'<%= pkg.title %>',
-	'<%= chuiName %>',
-	'Copyright ' + gutils.date("yyyy") + ' Sourcebits www.sourcebits.com',
-	'License: <%= pkg.licences[0].type %>',
-	'Version: <%= pkg.version %>',
-	'*/\n'].join('\n');
+  '    pO\\',
+  '   6  /\\',
+  '     /OO\\',
+  '    /OOOO\\',
+  '   /OOOOOO\\',
+  '  (OOOOOOOO)',
+  '   \\:~==~:/',
+  '',
+  '<%= pkg.title %>',
+  '<%= chuiName %>',
+  'Copyright ' + gutils.date("yyyy") + ' Sourcebits www.sourcebits.com',
+  'License: <%= pkg.licences[0].type %>',
+  'Version: <%= pkg.version %>',
+  '*/\n'].join('\n');
 
 // Define header for minfied ChUI files:
 var chuiHeaderMin = ['/*',
-	'<%= pkg.title %>',
-	'<%= chuiName %>',
-	'Copyright ' + gutils.date("yyyy") + ' Sourcebits www.sourcebits.com',
-	'License: <%= pkg.licences[0].type %>',
-	'Version: <%= pkg.version %>',
-	'*/\n'].join('\n');
+  '<%= pkg.title %>',
+  '<%= chuiName %>',
+  'Copyright ' + gutils.date("yyyy") + ' Sourcebits www.sourcebits.com',
+  'License: <%= pkg.licences[0].type %>',
+  'Version: <%= pkg.version %>',
+  '*/\n'].join('\n');
 
 // Define header for examples and demos:
 var htmlHeader = ['<!DOCTYPE html>',
@@ -61,27 +61,27 @@ var htmlHeader = ['<!DOCTYPE html>',
 
 // Process, minify and output LESS:
 gulp.task('less', function () {
-	osTypes.forEach(function(ctx, idx) {
-		gulp.src('src/themes/' + ctx + '/main.less')
-			.pipe(less())
-			.pipe(rename('chui-' + ctx + '-' + pkg.version + '.css'))
-		  .pipe(header(chuiHeader, { pkg : pkg, chuiName: chui[idx] }))
-			.pipe(gulp.dest(pkg.projectPath + 'chui/')).pipe(minifyCSS({}))
-		  .pipe(header(chuiHeaderMin, { pkg : pkg, chuiName: chui[idx] }))
-	    .pipe(rename('chui-' + ctx + '-' + pkg.version + '.min.css'))
-	    .pipe(gulp.dest(pkg.projectPath + './chui/'));;		
-	});
+  osTypes.forEach(function(ctx, idx) {
+    gulp.src('src/themes/' + ctx + '/main.less')
+      .pipe(less())
+      .pipe(rename('chui-' + ctx + '-' + pkg.version + '.css'))
+      .pipe(header(chuiHeader, { pkg : pkg, chuiName: chui[idx] }))
+      .pipe(gulp.dest(pkg.projectPath + 'chui/')).pipe(minifyCSS({}))
+      .pipe(header(chuiHeaderMin, { pkg : pkg, chuiName: chui[idx] }))
+      .pipe(rename('chui-' + ctx + '-' + pkg.version + '.min.css'))
+      .pipe(gulp.dest(pkg.projectPath + './chui/'));;   
+  });
 });
 
 // Concat, minify and output JavaScript:
 gulp.task('js', function () {
-	var chuijs_start = [
-		'\(function\(\$\) {',
-		'  \'use strict\';'
-	].join('\n');
-	var chuijs_end = '\n\}\)\(window\.jQuery\);';
+  var chuijs_start = [
+    '\(function\(\$\) {',
+    '  \'use strict\';'
+  ].join('\n');
+  var chuijs_end = '\n\}\)\(window\.jQuery\);';
 
-	gulp.src([
+  gulp.src([
     'src/chui/utils.js', 
     'src/chui/events.js', 
     'src/chui/detectors.js', 
@@ -108,17 +108,18 @@ gulp.task('js', function () {
     'src/chui/scroll.js', 
     'src/chui/tabbar.js', 
     'src/chui/templates.js', 
-    'src/chui/search.js'
-	])
-	  .pipe(concat("chui-" + pkg.version + ".js"))
-	  .pipe(replace(/\(function\(\$\) {\n^.*\'use strict\';/img, ''))
-	  .pipe(replace(/\}\)\(window\.jQuery\);/img, ''))
-	  .pipe(replace(/\n\n\n/img, ''))
-	  .pipe(replace(/\n\n/img, '\n'))
-	  .pipe(header(chuijs_start))
-	  .pipe(footer(chuijs_end))
-	  .pipe(header(chuiHeader, { pkg : pkg, chuiName: chui[3] }))
-	  .pipe(gulp.dest(pkg.projectPath + 'chui/'))
+    'src/chui/search.js', 
+    'src/chui/carousel.js'
+  ])
+    .pipe(concat("chui-" + pkg.version + ".js"))
+    .pipe(replace(/\(function\(\$\) {\n^.*\'use strict\';/img, ''))
+    .pipe(replace(/\}\)\(window\.jQuery\);/img, ''))
+    .pipe(replace(/\n\n\n/img, ''))
+    .pipe(replace(/\n\n/img, '\n'))
+    .pipe(header(chuijs_start))
+    .pipe(footer(chuijs_end))
+    .pipe(header(chuiHeader, { pkg : pkg, chuiName: chui[3] }))
+    .pipe(gulp.dest(pkg.projectPath + 'chui/'))
     .pipe(uglify())
     .pipe(header(chuiHeaderMin, { pkg : pkg, chuiName: chui[3] }))
     .pipe(rename("chui-" + pkg.version + ".min.js"))
@@ -127,44 +128,44 @@ gulp.task('js', function () {
 
 // Create examples & demos (ltr or rtl):
 gulp.task('examples', function() {
-	var langDir = '';
-	var rtl = gulp.env.dir === 'rtl';
-	var dir = 'ltr';
-	var prefix = '';
-	if (rtl) {
-		prefix = 'rtl-';
-		dir = 'rtl';
-		langDir =  ' dir="rtl"';
-		// Copy out rtl images:
-		gulp.src('src/rtl-images/**/*')
-			.pipe(gulp.dest('rtl-images/'));
-		// Copy out regular images & data:`
-		gulp.run('copy');
-	}
-	osTypes.forEach(function(ctx, idx) {
-		gulp.src('src/' + prefix + 'examples/**/*')
-	  	.pipe(header(htmlHeader, { pkg : pkg, osType : osTypes[idx], osName : osNames[idx], dir : prefix[dir], langDir : langDir }))
-			.pipe(gulp.dest(prefix + 'examples-' + ctx + '/'));
-  	gulp.src('src/' + prefix + 'demo/*.html')
-		  .pipe(header(htmlHeader, { pkg : pkg, osType : osTypes[idx], osName : osNames[idx], dir : prefix[dir], langDir : langDir }))
-		  .pipe(rename(prefix + 'demo-' + ctx + '.html'))
-			.pipe(gulp.dest(prefix + 'demo/'));
-	});
+  var langDir = '';
+  var rtl = gutils.env.dir === 'rtl';
+  var dir = 'ltr';
+  var prefix = '';
+  if (rtl) {
+    prefix = 'rtl-';
+    dir = 'rtl';
+    langDir =  ' dir="rtl"';
+    // Copy out rtl images:
+    gulp.src('src/rtl-images/**/*')
+      .pipe(gulp.dest('rtl-images/'));
+    // Copy out regular images & data:`
+    gulp.run('copy');
+  }
+  osTypes.forEach(function(ctx, idx) {
+    gulp.src('src/' + prefix + 'examples/**/*')
+      .pipe(header(htmlHeader, { pkg : pkg, osType : osTypes[idx], osName : osNames[idx], dir : prefix[dir], langDir : langDir }))
+      .pipe(gulp.dest(prefix + 'examples-' + ctx + '/'));
+    gulp.src('src/' + prefix + 'demo/*.html')
+      .pipe(header(htmlHeader, { pkg : pkg, osType : osTypes[idx], osName : osNames[idx], dir : prefix[dir], langDir : langDir }))
+      .pipe(rename(prefix + 'demo-' + ctx + '.html'))
+      .pipe(gulp.dest(prefix + 'demo/'));
+  });
 });
 
 // Copy out media:
 gulp.task('copy', function() {
   gulp.src('src/images/**/*')
-  	.pipe(gulp.dest('images/'));
+    .pipe(gulp.dest('images/'));
   gulp.src('src/data/**/*')
-  	.pipe(gulp.dest('data/'));
+    .pipe(gulp.dest('data/'));
 });
 
 // JSHint:
 gulp.task('jshint', function() {
-	gulp.src("chui-" + pkg.version + ".js")
-	  .pipe(jshint())
-	  .pipe(jshint.reporter('default'));
+  gulp.src("chui-" + pkg.version + ".js")
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
 });
 
 /* Define default task:
@@ -174,24 +175,12 @@ To build right-to-left examples, use:
   gulp --dir rtl
 
 */
-gulp.task('default', function () {
-	gulp.run('less');
-	gulp.run('js');
-	gulp.run('jshint');
-	gulp.run('copy');
-	gulp.run('examples');
-});
+gulp.task('default', ['less','js','jshint','copy','examples']);
 
-gulp.task('chui', function() {
-	gulp.run('less');
-	gulp.run('js');
-	gulp.run('jshint');
-});
+gulp.task('chui', ['less','js','jshint']);
 
 
 // Watch LESS files and generate CSS:
 gulp.task('watch', function() {
-	gulp.watch(['src/themes/**/*.less'], function() {
-		gulp.run('less');
-	});
+  gulp.watch('src/themes/**/*.less', ['less']);
 });
