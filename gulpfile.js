10ff53ffd7335c62796379e88caa29a1e55ada2a
sourcebits-rbiggs <robert.biggs@sourcebits.com>
2014-05-14 11:28:20 -0700

    Updated Gulpjs to create dist files.

--- gulpfile.js
+++ gulpfile.js
@@ -69,9 +69,11 @@ var less_for = function (os, idx) {
       .pipe(rename('chui-' + os + '-' + pkg.version + '.css'))
       .pipe(header(chuiHeader, { pkg : pkg, chuiName: chui[idx] }))
       .pipe(gulp.dest(pkg.projectPath + 'chui/')).pipe(minifyCSS({}))
+      .pipe(gulp.dest(pkg.projectPath + 'dist/')).pipe(minifyCSS({}))
       .pipe(header(chuiHeaderMin, { pkg : pkg, chuiName: chui[idx] }))
       .pipe(rename('chui-' + os + '-' + pkg.version + '.min.css'))
-      .pipe(gulp.dest(pkg.projectPath + './chui/'));;   
+      .pipe(gulp.dest(pkg.projectPath + './chui/'))
+      .pipe(gulp.dest(pkg.projectPath + './dist/')); 
 }
 
 // Process, minify and output LESS:
@@ -129,10 +131,12 @@ gulp.task('js', function () {
     .pipe(footer(chuijs_end))
     .pipe(header(chuiHeader, { pkg : pkg, chuiName: chui[3] }))
     .pipe(gulp.dest(pkg.projectPath + 'chui/'))
+    .pipe(gulp.dest(pkg.projectPath + './dist/'))
     .pipe(uglify())
     .pipe(header(chuiHeaderMin, { pkg : pkg, chuiName: chui[3] }))
     .pipe(rename("chui-" + pkg.version + ".min.js"))
-    .pipe(gulp.dest(pkg.projectPath + 'chui/'));;
+    .pipe(gulp.dest(pkg.projectPath + 'chui/'))
+    .pipe(gulp.dest(pkg.projectPath + './dist/'));
 });
 
 // Copy out media:
