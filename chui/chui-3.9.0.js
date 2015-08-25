c37e493c783e69f39b5407a8b939ecbdad171dd1
chocolatechipui@sourcebits.com <wobba@Roberts-MacBook.local>
2015-08-25 07:10:11 -0700

    Fixed UITabbar to set proper selected state.
    The tab bar was not setting the proper nav and article when other than
    the first tab as default.

--- chui/chui-3.9.0.js
+++ chui/chui-3.9.0.js
@@ -2318,7 +2318,7 @@ if(window.jQuery) {
       var articles = $('article');
       for (var i = 0; i < settings.tabs; i++) {
         tabbar += '<button class="' + settings.icons[i];
-        if (settings.selected === i+1) {
+        if (settings.selected === i) {
           tabbar += ' selected';
         }
         tabbar += '">' + icon + '<label>' + settings.labels[i] + '</label></button>';
@@ -2334,9 +2334,11 @@ if(window.jQuery) {
       });
       $('nav').removeClass('current').addClass('next');
       $('#global-nav').removeClass('next');
-      $('nav').eq(settings.selected).removeClass('next').addClass('current');
-      $('article').removeClass('current').addClass('next');
-      $('article').eq(settings.selected-1).removeClass('next').addClass('current');
+      // $('nav').eq(settings.selected).removeClass('next').addClass('current');
+      // $('article').removeClass('current').addClass('next');
+      
+      $('article').eq(settings.selected).removeClass('next').addClass('current');
+      $('article').eq(settings.selected).prev('nav').removeClass('next').addClass('current');
 
       // Setup events on tabs:
       var tabButtonTap = 'singletap';
