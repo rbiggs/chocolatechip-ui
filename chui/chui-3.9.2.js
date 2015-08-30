5d7a58670c745e16b26f45a4b175f3f300524005
chocolatechipui@sourcebits.com <wobba@Roberts-MacBook.local>
2015-08-30 01:27:22 -0700

    Fixed bug in pub/sub.
    Had boolean check in pub/sub that was causing ChocolateChipJS to exit
    without load the rest of ChUI.

--- chui/chui-3.9.2.js
+++ chui/chui-3.9.2.js
@@ -631,54 +631,56 @@ if(window.jQuery) {
   });
 
 
-  if (window.chocolatechipjs) return;
-  $.extend({
-    subscriptions : {},
 
-    // Topic: string defining topic: /some/topic
-    // Data: a string, number, array or object.
-    subscribe : function (topic, callback) {
-      if (!$.subscriptions[topic]) {
-        $.subscriptions[topic] = [];
-      }
-      var token = ($.Uuid());
-      $.subscriptions[topic].push({
-        token: token,
-        callback: callback
-      });
-      return token;
-    },
+  if (!window.chocolatechipjs) {
+    $.extend({
+      subscriptions : {},
 
-    unsubscribe : function ( token ) {
-      setTimeout(function() {
-        for (var m in $.subscriptions) {
-          if ($.subscriptions[m]) {
-              for (var i = 0, len = $.subscriptions[m].length; i < len; i++) {
-                  if ($.subscriptions[m][i].token === token) {
-                    $.subscriptions[m].splice(i, 1);
-                    return token;
-                  }
-              }
+      // Topic: string defining topic: /some/topic
+      // Data: a string, number, array or object.
+      subscribe : function (topic, callback) {
+        if (!$.subscriptions[topic]) {
+          $.subscriptions[topic] = [];
+        }
+        var token = ($.Uuid());
+        $.subscriptions[topic].push({
+          token: token,
+          callback: callback
+        });
+        return token;
+      },
+
+      unsubscribe : function ( token ) {
+        setTimeout(function() {
+          for (var m in $.subscriptions) {
+            if ($.subscriptions[m]) {
+                for (var i = 0, len = $.subscriptions[m].length; i < len; i++) {
+                    if ($.subscriptions[m][i].token === token) {
+                      $.subscriptions[m].splice(i, 1);
+                      return token;
+                    }
+                }
+            }
           }
-        }
-        return false;
-      });
-    },
+          return false;
+        });
+      },
 
-    publish : function ( topic, args ) {
-      if (!$.subscriptions[topic]) {
-        return false;
-      }
-      setTimeout(function () {
-        var len = $.subscriptions[topic] ? $.subscriptions[topic].length : 0;
-        while (len--) {
-          $.subscriptions[topic][len].callback(topic, args);
+      publish : function ( topic, args ) {
+        if (!$.subscriptions[topic]) {
+          return false;
         }
+        setTimeout(function () {
+          var len = $.subscriptions[topic] ? $.subscriptions[topic].length : 0;
+          while (len--) {
+            $.subscriptions[topic][len].callback(topic, args);
+          }
+          return true;
+        });
         return true;
-      });
-      return true;
-   }
-  });
+     }
+    });
+  }
 
 
   ////////////////////////////////////
